308cfd130018235d8226b406f754132a414f65b3
aylingyoren <aylingyoren21@gmail.com>
2021-11-23 15:19:01 +0300

    implement SideMenu, add favorites, move PageHeader

---
+++ src/components/Favorites/Favorites.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Favorites.css';
+
+function Favorites(): JSX.Element {
+    return (
+        <div className='favorites'>
+            <h1>Favorites</h1>
+        </div>
+    );
+}
+
+export default Favorites;
