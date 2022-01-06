2eb9d5bbf4b9936018cb635ed420fdfa81413d9b
aylingyoren <aylingyoren21@gmail.com>
2022-01-06 17:49:29 +0300

    return props to BeerCard n type them

--- src/components/BeerCard/BeerCard.tsx
+++ src/components/BeerCard/BeerCard.tsx
@@ -1,11 +1,40 @@
-import React, { useState } from "react";
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import { FavoriteContext } from "../../API/FavoriteContext";
 import { Beer } from "../../API/interface";
 import "./BeerCard.css";
 
-// specify the props!!!!!
+type BeerCardProps = {
+  key: number
+  item: Beer;
+};
 
-function BeerCard(props: any): JSX.Element {
-  return <div className="beer-card">{props.children}</div>;
+function BeerCard(props: BeerCardProps): JSX.Element {
+  const [favorite, addToFavorite, removeFromFavorite,] =
+    useContext(FavoriteContext);
+
+  const { item } = props;
+
+  return (
+    <div className="beer-card">
+      <img className="beer-img" src={item.image_url} />
+      <div className="beer-plate">
+        <h2 className="beer-name">{item.name}</h2>
+        <p className="beer-tagline">{item.tagline}</p>
+        <Link to={`/beerPage/${item.id}`}>
+          <button className="beer-btn beer-open">Open</button>
+        </Link>
+        <button
+          onClick={(event: React.MouseEvent<HTMLElement>) =>
+            item.isFav ? removeFromFavorite(item, item.id) : addToFavorite(item)
+          }
+          className="beer-btn beer-fav"
+        >
+          {favorite.find(el => el.id === item.id) ? "Remove Favorite" : "Favorite"}
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default BeerCard;
--- src/components/BeerList/BeerList.tsx
+++ src/components/BeerList/BeerList.tsx
@@ -1,25 +1,18 @@
-import { useContext } from "react";
-import { Link } from "react-router-dom";
 import { useSearchPage } from "../../hooks/useSearchPage";
 import { Beer } from "../../API/interface";
-import { FavoriteContext } from "../../API/FavoriteContext";
 import BeerCard from "../BeerCard";
 import "./BeerList.css";
 
-type BeerlistProps = {
+type BeerListProps = {
   search: string;
 };
 
-function BeerList(props: BeerlistProps): JSX.Element {
+function BeerList(props: BeerListProps): JSX.Element {
   const { beerList, loading, error } = useSearchPage(
     "https://api.punkapi.com/v2/beers?page=2&per_page=80"
   );
 
   const { search } = props;
-
-  const [favorite, addToFavorite, removeFromFavorite,] =
-    useContext(FavoriteContext);
-  console.log(favorite);
 
   if (loading) {
     return <div> Loading... </div>;
@@ -42,24 +35,10 @@ function BeerList(props: BeerlistProps): JSX.Element {
         })
         .map((item: Beer) => {
           return (
-            <BeerCard key={item.id}>
-              <img className="beer-img" src={item.image_url} />
-              <div className="beer-plate">
-                <h2 className="beer-name">{item.name}</h2>
-                <p className="beer-tagline">{item.tagline}</p>
-                <Link to={`/beerPage/${item.id}`}>
-                  <button className="beer-btn beer-open">Open</button>
-                </Link>
-                <button
-                  onClick={(event: React.MouseEvent<HTMLElement>) =>
-                    item.isFav ? removeFromFavorite(item, item.id) : addToFavorite(item)
-                  }
-                  className="beer-btn beer-fav"
-                >
-                  {favorite.find(el => el.id === item.id) ? "Remove Favorite" : "Favorite"}
-                </button>
-              </div>
-            </BeerCard>
+            <BeerCard 
+              key={item.id}
+              item={item}
+            />
           );
         })}
     </div>
